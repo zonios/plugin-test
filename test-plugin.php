ee78f72646ae7ebcc963489ba842a263af97dcc8
Sterio Zonios <sterio.zonios@hotmail.com>
2019-04-09 11:17:03 +0200

    first commit

---
+++ test-plugin.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name:     test-plugin
+ * Description:     Mon test de plugin
+ * Author:          Sterio
+ * Text Domain:     test-plugin
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         test-plugin
+ */
+// Your code starts here.
